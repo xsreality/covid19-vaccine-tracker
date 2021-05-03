f11ae3fe48ad87c28eb685583c1abc1383bb7775
Abhinav Sonkar <abhinav@axual.com>
2021-05-03 20:55:29 +0200

    Fix the BotBackend dependency.

--- src/main/java/com/abhinav/covid19/vaccinetracker/bot/BotService.java
+++ src/main/java/com/abhinav/covid19/vaccinetracker/bot/BotService.java
@@ -1,9 +1,5 @@
 package com.abhinav.covid19.vaccinetracker.bot;
 
-import com.abhinav.covid19.vaccinetracker.model.VaccineCenters;
-
 public interface BotService {
-    String format(String userId, VaccineCenters vaccineCenters);
-
-    void send(String message);
+    void notify(String userId, String message);
 }
