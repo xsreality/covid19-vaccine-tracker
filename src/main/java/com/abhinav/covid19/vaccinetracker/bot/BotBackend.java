d4a1dd9b128c0524ab5b7397e92b2564ea385506
Abhinav Sonkar <abhinav@axual.com>
2021-05-03 20:55:29 +0200

    Fix the BotBackend dependency.

--- src/main/java/com/abhinav/covid19/vaccinetracker/bot/BotBackend.java
+++ src/main/java/com/abhinav/covid19/vaccinetracker/bot/BotBackend.java
@@ -1,7 +1,6 @@
 package com.abhinav.covid19.vaccinetracker.bot;
 
 import com.abhinav.covid19.vaccinetracker.model.UserRequest;
-import com.abhinav.covid19.vaccinetracker.model.VaccineCenters;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,22 +14,14 @@ public class BotBackend {
     @Value("{user.requests.topic")
     private String userRequestsTopic;
 
-    private final BotService botService;
     private final KafkaTemplate<String, UserRequest> kafkaTemplate;
 
-    public BotBackend(BotService botservice, KafkaTemplate<String, UserRequest> kafkaTemplate) {
-        this.botService = botservice;
+    public BotBackend(KafkaTemplate<String, UserRequest> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     public void acceptUserRequest(String userId, String pincode) {
         UserRequest request = new UserRequest(userId, pincode);
         kafkaTemplate.send(userRequestsTopic, userId, request);
-    }
-
-    public void notifyUser(String userId, VaccineCenters vaccineCenters) {
-        log.info("Notifying user {} about their vaccine centers", userId);
-        String message = botService.format(userId, vaccineCenters);
-        botService.send(message);
     }
 }
