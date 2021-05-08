5cf624a24ee5c8259f9335bafbdc95809e04941a
Abhinav Sonkar <xsabhinav@gmail.com>
2021-05-09 00:38:09 +0200

    0.1.5: Added localization support for acknowledgment text.

--- src/main/java/org/covid19/vaccinetracker/persistence/mariadb/repository/StateRepository.java
+++ src/main/java/org/covid19/vaccinetracker/persistence/mariadb/repository/StateRepository.java
@@ -1,9 +1,15 @@
 package org.covid19.vaccinetracker.persistence.mariadb.repository;
 
 import org.covid19.vaccinetracker.persistence.mariadb.entity.State;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StateRepository extends CrudRepository<State, Integer> {
+    @Query("SELECT s FROM State s " +
+            "INNER JOIN District d ON d.state = s " +
+            "INNER JOIN Pincode p ON p.district = d " +
+            "WHERE p.pincode = :pincode")
+    State findByPincode(String pincode);
 }
