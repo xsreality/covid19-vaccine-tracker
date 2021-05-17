4594f987104439ab74927ebb158e10f2abbc3560
Abhinav Sonkar <xsabhinav@gmail.com>
2021-05-17 22:36:56 +0200

    Added DB cleanup job for expired vaccine centers and sessions.

--- src/main/java/org/covid19/vaccinetracker/utils/Utils.java
+++ src/main/java/org/covid19/vaccinetracker/utils/Utils.java
@@ -135,6 +135,12 @@ public class Utils {
         return dateTime.format(dtf);
     }
 
+    public static String yesterdayIST() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        ZonedDateTime dateTime = ZonedDateTime.now(ZoneId.of(INDIA_TIMEZONE)).minusDays(1L);
+        return dateTime.format(dtf);
+    }
+
     public static boolean dayOld(String lastNotifiedAt) {
         ZonedDateTime notifiedAt = dateFromString(lastNotifiedAt);
         ZonedDateTime currentTime = ZonedDateTime.now(ZoneId.of(INDIA_TIMEZONE));
