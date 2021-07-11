d5a0620c4b83626b6a668d22547efbe9bc1b5aef
Abhinav Sonkar <xsabhinav@gmail.com>
2021-07-12 00:46:14 +0200

    Only include recent-most sessions when generating causes and alternatives.

--- src/main/java/org/covid19/vaccinetracker/notifications/absentalerts/AbsentAlertAnalyzer.java
+++ src/main/java/org/covid19/vaccinetracker/notifications/absentalerts/AbsentAlertAnalyzer.java
@@ -7,33 +7,33 @@ import org.covid19.vaccinetracker.utils.Utils;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparing;
 import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
 
 /**
- * Business logic ot analyze why a user has not received any alerts.
+ * Analyze why a user has not received any alerts.
  */
 @Component
 public class AbsentAlertAnalyzer {
+
     public AbsentAlertCause analyze(AbsentAlertSource source, List<CenterSession> sessions) {
         AbsentAlertCause absentAlertCause = AbsentAlertCause.builder()
                 .userId(source.getUserId())
                 .pincode(source.getPincode())
                 .build();
 
-        List<CenterSession> relevantSessions = sessions
-                .stream()
-                .filter(relevantSessionPredicate(source))
-                .collect(Collectors.toList());
-
-        List<CenterSession> alternativeSessions = sessions
-                .stream()
-                .filter(relevantSessionPredicate(source).negate())
-                .collect(Collectors.toList());
+        List<CenterSession> relevantSessions = sessions.stream().filter(relevantSessionPredicate(source)).collect(Collectors.toList());
+        List<CenterSession> alternativeSessions = sessions.stream().filter(relevantSessionPredicate(source).negate()).collect(Collectors.toList());
 
         // set last notified in cause
         ofNullable(source.getLatestNotification())
@@ -42,32 +42,41 @@ public class AbsentAlertAnalyzer {
                         userNotification -> absentAlertCause.setLastNotified(
                                 String.format("You were last notified for the pincode %s on %s",
                                         source.getPincode(), Utils.convertToIST(userNotification.getNotifiedAt()))),
-                        () -> absentAlertCause.setLastNotified(String.format("You have not received any notification for pincode %s", source.getPincode())));
+                        () -> absentAlertCause.setLastNotified(
+                                String.format("You have not received any notification for pincode %s", source.getPincode())));
 
 
-        if (relevantSessions.isEmpty()) {
-            // no session has opened for user pincode before, include a helpful message
-            absentAlertCause.addCause(String.format("No centers found for pincode %s.", source.getPincode()));
-        } else {
+        if (!relevantSessions.isEmpty()) {
             // sessions have opened for user pincode before, include them in causes
             relevantSessions
-                    .forEach(session -> {
-                        String cause = String.format("%s (%s %s) last had open slots for %s on %s.",
-                                session.getCenterName(), session.getDistrictName(),
-                                session.getPincode(), session.getSessionVaccine(), session.getSessionDate());
-                        absentAlertCause.addCause(cause);
-                    });
+                    .stream()
+                    .collect(groupingBy(CenterSession::getCenterName, maxBy(comparing(o -> toLocalDate(o.getSessionDate())))))
+                    .values()
+                    .stream()
+                    .flatMap(Optional::stream)
+                    .forEach(currentSession -> absentAlertCause.addCause(
+                            String.format("%s (%s %s) last had open slots for %s on %s",
+                                    currentSession.getCenterName(), currentSession.getDistrictName(),
+                                    currentSession.getPincode(), currentSession.getSessionVaccine(), currentSession.getSessionDate())));
+        } else {
+            // no session has opened for user pincode before, include a helpful message
+            absentAlertCause.addCause(String.format("No centers found for pincode %s.", source.getPincode()));
         }
+
         // check if sessions have opened outside user preferences for same pincode
         // and include them in causes
         if (!alternativeSessions.isEmpty()) {
             alternativeSessions
-                    .forEach(session -> {
-                        String alternative = String.format("%s (%s %s) last had open slots for %s on %s.",
-                                session.getCenterName(), session.getDistrictName(),
-                                session.getPincode(), session.getSessionVaccine(), session.getSessionDate());
-                        absentAlertCause.addAlternative(alternative);
-                    });
+                    .stream()
+                    .collect(groupingBy(CenterSession::getCenterName, maxBy(comparing(o -> toLocalDate(o.getSessionDate())))))
+                    .values()
+                    .stream()
+                    .flatMap(Optional::stream)
+                    .filter(session -> matchingAgePreference(source.getAge(), session.getMinAge()))
+                    .forEach(session -> absentAlertCause.addAlternative(
+                            String.format("%s (%s %s) last had open slots for %s on %s",
+                                    session.getCenterName(), session.getDistrictName(),
+                                    session.getPincode(), session.getSessionVaccine(), session.getSessionDate())));
         }
         return absentAlertCause;
     }
@@ -99,5 +108,10 @@ public class AbsentAlertAnalyzer {
             // age pref is both so just return true
             return true;
         }
+    }
+
+    private LocalDate toLocalDate(String ddMMyyyy) {
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        return LocalDate.parse(ddMMyyyy, inputFormat);
     }
 }
---
+++ src/test/java/org/covid19/vaccinetracker/notifications/absentalerts/AbsentAlertsAnalyzerTest.java
@@ -0,0 +1,79 @@
+package org.covid19.vaccinetracker.notifications.absentalerts;
+
+import org.covid19.vaccinetracker.model.CenterSession;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AbsentAlertsAnalyzerTest {
+    @Test
+    public void verifyAnalyzeBehaviour() {
+        List<CenterSession> sessions = createSessions();
+        AbsentAlertSource source = AbsentAlertSource.builder()
+                .userId("1234").pincode("700014").age("45+").vaccine("Covaxin").dose("Dose 2").build();
+        AbsentAlertAnalyzer analyzer = new AbsentAlertAnalyzer();
+        final AbsentAlertCause cause = analyzer.analyze(source, sessions);
+        log.info("Cause is {}", cause.getCauses());
+        log.info("Alternative is {}", cause.getAlternatives());
+    }
+
+    private List<CenterSession> createSessions() {
+        return List.of(
+                CenterSession.builder()
+                        .centerName("NRS PPU MCH COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(18).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("NRS PPU MCH COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("UPHC-54").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("05-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("UPHC-53").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("05-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("UPHC-60").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("05-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BR Singh Railway Hosp COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BR Singh Railway Hosp COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(18).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("CNMCH PPU COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("CNMCH PPU COVAXIN").districtName("Kolkata").pincode("700014")
+                        .minAge(18).sessionDate("12-06-2021").sessionVaccine("COVAXIN")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BELLE VUE CLINIC").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("17-07-2021").sessionVaccine("COVISHIELD")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BELLE VUE CLINIC").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("16-07-2021").sessionVaccine("COVISHIELD")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BELLE VUE CLINIC").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("15-07-2021").sessionVaccine("COVISHIELD")
+                        .build(),
+                CenterSession.builder()
+                        .centerName("BELLE VUE CLINIC").districtName("Kolkata").pincode("700014")
+                        .minAge(45).sessionDate("14-07-2021").sessionVaccine("COVISHIELD")
+                        .build()
+        );
+    }
+}
